68b4e754f6d960266f0107e38e337c0caec094b5
Abram <tacksontiger@gmail.com>
2025-03-14 00:00:04 +0300

    correction commit
    The code had disappeared while switching to another branch but i just retype it

--- userModule/src/userPackage/UserData.java
+++ userModule/src/userPackage/UserData.java
@@ -1,4 +1,21 @@
 package userPackage;
 
 public class UserData {
+        private String name;
+        private String email;
+
+        public UserData(String name, String email) {
+            this.name = name;
+            this.email = email;
+        }
+    public String getName() {
+        return name;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void displayUserInfo(){
+        System.out.println("The user name is "+name);
+        System.out.println("The user Email is "+email);
+    }
 }
