0b8c7f46370c7ef238a826018501090b74441e35
Abram <tacksontiger@gmail.com>
2025-03-19 12:32:08 +0300

    Commit #1
    Modified Main.java

--- itemModule/src/itemPackage/Item.java
+++ itemModule/src/itemPackage/Item.java
@@ -1,5 +1,26 @@
 package itemPackage;
 
 public class Item {
-    
+    private String item1;
+    private double item1Price;
+    private String  item2;
+    private double item2Price;
+
+    public Item(String item1, double item1Price, String item2, double item2Price) {
+        this.item1 = item1;
+        this.item1Price = item1Price;
+        this.item2 = item2;
+        this.item2Price = item2Price;
+    }
+    public double getItem1Price() {
+        return item1Price;
+    }
+    public double getItem2Price() {
+        return item2Price;
+    }
+
+    public void add(){
+        double newItem = item1Price + item2Price;
+        System.out.println("The addition of the new item total is "+newItem+"$");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import userPackage.*;
 import itemPackage.*;
 public class Main {
     public static void main(String[] args) {
-
+        UserData obj = new UserData("Tackson","tackson@mail.com");
+        obj.displayUserInfo();
+        System.out.println();
+        Item obj2 = new Item("Hp",300,"Dell",400);
+        obj2.add();
     }
 }
